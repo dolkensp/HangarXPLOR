2966579c3e001e62672aeeeed3388b0a07b25276
Jan Krüger <jan@krueger-jan.de>
2018-03-27 14:08:11 +0200

    PR #7 but branch is now based on the master

--- web_resources/HangarXPLOR.SearchBox.js
+++ web_resources/HangarXPLOR.SearchBox.js
@@ -2,15 +2,26 @@
 var HangarXPLOR = HangarXPLOR || {};
 
 // Render a searchbox
-HangarXPLOR.SearchBox = function(value)
+HangarXPLOR.SearchBox = function()
 {
   if (HangarXPLOR.logsEnabled) {
     console.log('Rendering SearchBox', value);
   }
-  
-  var $searchBox = null;
-  
-  // TODO: Implement Search Box
-  
-  return $searchBox;
+
+  const $searchBox = $('<input>', { id: 'searchInput', class: 'js-custom-search' });
+  const $searchBoxMirror = $('<input>', { class: 'js-custom-search js-custom-search-complete', disabled: "disabled" });
+
+  $searchBox.keyup(function(event) {
+      HangarXPLOR.Render();
+      HangarXPLOR.RefreshPager();
+  });
+
+  $searchBox.keydown(function(event) {
+      if(event.keyCode === 9) {
+          event.preventDefault();
+          $('#searchInput').val($('.js-custom-search-complete').val());
+      }
+  })
+
+  return [$searchBox[0], $searchBoxMirror[0]];
 }
