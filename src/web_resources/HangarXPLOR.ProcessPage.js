8b7efcf7fefb7fa625892ac606ec6b67f84826f7
Peter Dolkens <github@ddrit.com>
2019-06-07 01:47:16 +0100

    Remove obsolete filter

--- src/web_resources/HangarXPLOR.ProcessPage.js
+++ src/web_resources/HangarXPLOR.ProcessPage.js
@@ -4,8 +4,6 @@ var HangarXPLOR = HangarXPLOR || {};
 // Pre-process all the items in a document, then load the next page, or render the UI
 HangarXPLOR.ProcessPage = function($page, pageNo)
 {
-  var $lists = $('.list-items', $page);
-  
   var $items = $('.list-items > li', $page);
       
   $items.each(HangarXPLOR.ProcessItem);
