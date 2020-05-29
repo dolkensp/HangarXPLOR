d9771469f97a1c8e31d10c45e49871a0f28a9c3f
Dilandau <dilandau@sheeris.ovh>
2020-05-29 18:48:17 +0200

    Fix Upgrade list

--- src/web_resources/HangarXPLOR.ProcessItem.js
+++ src/web_resources/HangarXPLOR.ProcessItem.js
@@ -146,7 +146,7 @@ HangarXPLOR.ProcessItem = function()
     this.isPackage = ($('.title:contains(Squadron 42 Digital Download)', this).length + $('.title:contains(Star Citizen Digital Download)', this).length) > 0;
     this.isShip = $ship.length == 1;
     this.isCombo = $ship.length > 1;
-    this.isUpgrade = (titleParts[0] == "Ship Upgrades");
+    this.isUpgrade = (titleParts[0] == "Ship Upgrades" || titleParts[0] == "Upgrade");
     this.isAddOn = (titleParts[0] == "Add-Ons");
     this.isTrophy = (titleParts[0] == "Trophy");
     this.isPoster = (titleParts[0] == "Posters");
