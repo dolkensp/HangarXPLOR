3f79b8572a1703211e282dc322b416ee3ae63cea
Peter Dolkens <github@ddrit.com>
2020-06-01 08:32:29 +0100

    Initial caching support TODO: Add Setting to disable caching TODO: Add manual cache purge function

---
+++ src/web_resources/HangarXPLOR.LoadCache.js
@@ -0,0 +1,25 @@
+
+var HangarXPLOR = HangarXPLOR || {};
+
+HangarXPLOR.LoadCache = function(callback)
+{
+  HangarXPLOR.Log('Load Cache');
+  
+  chrome.storage.local.get(null, (cache) => {
+    
+    if (HangarXPLOR._cacheHash == HangarXPLOR._activeHash && cache['cache:count'] > 0)
+    {
+      HangarXPLOR._fromCache = true;
+      var cacheItems = [];
+      
+      for (var i = 0; i < cache['cache:count']; i++) { HangarXPLOR.ProcessItem.apply($(cache['cache:' + i])[0]) }
+      
+      HangarXPLOR.DrawUI();
+      return;
+    }
+    
+    HangarXPLOR._fromCache = false;
+    if (typeof callback === 'function') callback.call(this);
+    
+  });
+}
--- src/web_resources/HangarXPLOR.SaveSettings.js
+++ src/web_resources/HangarXPLOR.SaveSettings.js
@@ -6,21 +6,22 @@ HangarXPLOR.SaveSettings = function()
   HangarXPLOR = HangarXPLOR || {};
   HangarXPLOR._feature = HangarXPLOR._feature || {};
   
-  var settings = settings || {};
-  settings._type = HangarXPLOR._type || 'All';
-  settings._sort = HangarXPLOR._sort || 'Purchased';
-  settings._pageNo = HangarXPLOR._pageNo || 1;
-  settings._pageCount = HangarXPLOR._pageCount || 10;
-  settings._logEnabled = HangarXPLOR._logEnabled || false;
+  var settings               = settings || {};
+  settings._type             = HangarXPLOR._type || 'All';
+  settings._sort             = HangarXPLOR._sort || 'Purchased';
+  settings._pageNo           = HangarXPLOR._pageNo || 1;
+  settings._pageCount        = HangarXPLOR._pageCount || 10;
+  settings._logEnabled       = HangarXPLOR._logEnabled || false;
+  settings._cacheHash        = HangarXPLOR._cacheHash || 0;
   
-  settings._feature_LTI = HangarXPLOR._feature.LTI || '';
-  settings._feature_Warbond = HangarXPLOR._feature.Warbond || '';
+  settings._feature_LTI      = HangarXPLOR._feature.LTI      || '';
+  settings._feature_Warbond  = HangarXPLOR._feature.Warbond  || '';
   settings._feature_Giftable = HangarXPLOR._feature.Giftable || '';
   settings._feature_Meltable = HangarXPLOR._feature.Meltable || '';
   settings._feature_Upgraded = HangarXPLOR._feature.Upgraded || '';
   settings._feature_Valuable = HangarXPLOR._feature.Valuable || '';
-  settings._feature_Reward = HangarXPLOR._feature.Reward || '';
-  settings._feature_Summary = HangarXPLOR._feature.Summary || 'cash';
+  settings._feature_Reward   = HangarXPLOR._feature.Reward   || '';
+  settings._feature_Summary  = HangarXPLOR._feature.Summary  || 'cash';
   
   HangarXPLOR.Log('Saved Settings', settings);
   
