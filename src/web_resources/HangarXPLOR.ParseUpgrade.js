c25dfeb365538d715f35e069968b57af48eae851
Peter Dolkens <peter.dolkens@clubspark.com>
2025-03-28 20:14:18 +1100

    Fix build warnings

--- src/web_resources/HangarXPLOR.ParseUpgrade.js
+++ src/web_resources/HangarXPLOR.ParseUpgrade.js
@@ -1,9 +1,8 @@
 
 var HangarXPLOR = HangarXPLOR || {};
 
-HangarXPLOR.ParseUpgrade = function(pledgeName)
+HangarXPLOR.ParseUpgrade = function()
 {
-  
   this.filters.is_upgrade = $('.js-upgrade-data', this).length > 0;
   
   if (this.filters.is_upgrade)
@@ -17,7 +16,6 @@ HangarXPLOR.ParseUpgrade = function(pledgeName)
     this.displayName = this.upgrade_data.match_items[0].name + ' to ' + this.upgrade_data.target_items[0].name
 
     var target_name = this.upgrade_data.target_items[0].name;
-    var found = false;
     var i, j;
     for (i = 0, j = HangarXPLOR._shipMatrix.length; i < j; i++) {
       if (target_name.toLowerCase().indexOf(HangarXPLOR._shipMatrix[i].name.toLowerCase()) > -1) {
@@ -25,7 +23,6 @@ HangarXPLOR.ParseUpgrade = function(pledgeName)
         HangarXPLOR.Log('Matched', HangarXPLOR._shipMatrix[i].name, 'in', target_name);
 
         target_name = (HangarXPLOR._shipMatrix[i].displayName || HangarXPLOR._shipMatrix[i].name);
-        found = true;
         if (HangarXPLOR._shipMatrix[i].thumbnail != undefined) {
           $('.basic-infos .image', this).css({ 'background-image': 'url("' + HangarXPLOR._shipMatrix[i].thumbnail + '")'});
         }
